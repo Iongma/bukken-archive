cfadf61dc3950695ce2180a807b52854f411710b
Iongma <msk0kopurei@gmail.com>
2021-06-15 15:26:51 +0900

    create model Prop class

--- app/models.py
+++ app/models.py
@@ -1,25 +1,13 @@
-import datetime
 from django.db import models
-from django.db.models.deletion import PROTECT
+from django.db.models.deletion import PROTECT, CASCADE
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.utils import timezone
+import datetime
 
-# Create your models here.
-class User(AbstractUser):
-    name = models.CharField(_('username'), max_length=254)
-    email = models.EmailField(max_length = 254, unique=True, null=False, blank=False)
-    password = models.CharField(_('password'), max_length=128)
-    like = models.ManyToManyField(Property, null=True, blank=True, on_delite=PROTECT) #models.ForeignKey(Question, on_delete=models.CASCADE)
-    is_staff = models.BooleanField(_('staff status'), default=False)
-    is_superuser = models.BooleanField(_('superuser status'), default=False)
-    join_date = models.DateTimeField(_('date joined'), default=timezone.now)
-    last_date = models.DateField(_('last login'), blank=True, null=True)
-
-    def __str__(self):
-        return self.name
 
 class Property(models.Model):
-    prop_name = models.CharField(max_length=254)
+    prop_name = models.CharField(max_length=254, default="sample")
     area = models.CharField(max_length=10)
     address = models.CharField(max_length=254)
     price = models.CharField(max_length=10)
@@ -35,10 +23,22 @@ class Property(models.Model):
     floor = models.CharField(max_length=100)
     age = models.CharField(max_length=10)
     detail_url = models.URLField()
-    like = models.ManyToManyField(get_user_model(), on_delete=models.PROTECT)#良いねの数に変換
+    # like = models.ManyToManyField(get_user_model(), on_delete=models.PROTECT)#良いねの数に変換
     pub_date = models.DateTimeField('date published')
-    pub_date = models.DateTimeField(_("date published"),auto_now_add=True)
+    pub_date = models.DateTimeField('date published',auto_now_add=True)
 
     def __str__(self):
         return self.prop_name
 
+class User(AbstractUser):
+    name = models.CharField('username', max_length=254, default="Iongma")
+    email = models.EmailField(max_length = 254, unique=True, null=False, blank=False)
+    password = models.CharField('password', max_length=128)
+    like = models.ManyToManyField(Property, blank=True)
+    is_staff = models.BooleanField('staff status', default=False)
+    is_superuser = models.BooleanField('superuser status', default=False)
+    join_date = models.DateTimeField('date joined', default=timezone.now)
+    last_date = models.DateField('last login', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -5,16 +5,16 @@ from django.views import generic
 from django.utils import timezone
 from .models import User
 
-class HomeView(generic.ListView):
-    template_name = 'app/home.html'
-    context_object_name = 'latest_question_list'
+# class HomeView(generic.ListView):
+#     template_name = 'app/home.html'
+#     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+#     def get_queryset(self):
+#         """
+#         Return the last five published questions (not including those set to be
+#         published in the future).
+#         """
+#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 # class UserView(generic.UserView):
 #     model = Question
